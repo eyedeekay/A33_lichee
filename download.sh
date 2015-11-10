6e9e1714028941fdc8f0710cf281fdf218e2f8a6
ernest0vm <arkangel_121@hotmail.com>
2015-11-10 12:15:29 -0600

    first commit

---
+++ download.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/ernest0vm/A33_lichee_brandy.git ./brandy
+git clone https://github.com/ernest0vm/A33_lichee_buildroot.git ./buildroot
+git clone https://github.com/ernest0vm/A33_lichee_linux-3.4.git ./linux-3.4
+git clone https://github.com/ernest0vm/A33_lichee_out.git ./out
+git clone https://github.com/ernest0vm/A33_lichee_tools.git ./tools
